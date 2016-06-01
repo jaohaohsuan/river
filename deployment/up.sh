2c4d713f1a40f596ef1e95a121e70a6e45567195
Henry Jao <jaohaohsuan@gmail.com>
2016-06-02 07:27:48 +0800

    fix kubectl wrong appy dir again

--- deployment/up.sh
+++ deployment/up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f manifests
+kubectl apply -f target/deployment/manifests
 IMG1="$(kubectl describe rc river | grep 'Image(s)' | awk '{print $2}')"
 IMG2="127.0.0.1:5000/inu/river:${version}"
 if [ "${IMG1}" != "${IMG2}" ]; then
