f1b7a739a768883a596a210128a0a004119fa26e
Henry Jao <jaohaohsuan@gmail.com>
2016-06-02 10:31:46 +0800

    bash syntax checking

--- deployment/up.sh
+++ deployment/up.sh
@@ -3,10 +3,13 @@ set -x
 
 kubectl get svc river --no-headers
 rc=$?
-if (( rc != 0)); then kubectl apply -f target/deployment/manifests/svc.yml; fi
+if (( rc != 0 )); then kubectl apply -f target/deployment/manifests/svc.yml; fi
 
 kubectl get rc river --no-headers
 rc=$?
-if (( rc != 0)); then kubectl apply -f target/deployment/manifests/rc.yml; fi
+if (( rc != 0 )); then kubectl apply -f target/deployment/manifests/rc.yml; fi
 
 kubectl rolling-update river --image=127.0.0.1:5000/inu/river:${version}
+
+
+
