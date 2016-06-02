e3ee25e77c4752d177a47805324cfc31a508e8c5
Henry Jao <jaohaohsuan@gmail.com>
2016-06-02 08:35:13 +0800

    refine rolling update

--- deployment/up.sh
+++ deployment/up.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -x
 
-kubectl apply -f target/deployment/manifests
-IMG1="$(kubectl describe rc river | grep 'Image(s)' | awk '{print $2}')"
-IMG2="127.0.0.1:5000/inu/river:${version}"
-if [ "${IMG1}" != "${IMG2}" ]; then
-    kubectl rolling-update river --image=${IMG2}
-fi
+kubectl get svc river --no-headers
+rc=$?
+if [[ $rc != 0 ]]; then kubectl apply -f target/deployment/manifests/svc.yml
+
+kubectl get rc river --no-headers
+rc=$?
+if [[ $rc != 0 ]]; then kubectl apply -f target/deployment/manifests/rc.yml
+
+kubectl rolling-update river --image=127.0.0.1:5000/inu/river:${version}
