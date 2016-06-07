e910f6b519cef76ebac54e5ad13bd7f31158ab46
Henry Jao <jaohaohsuan@gmail.com>
2016-06-07 18:41:36 +0800

    try fix rc.yml hasn't been update

--- deployment/up.sh
+++ deployment/up.sh
@@ -5,9 +5,11 @@ kubectl get svc river --no-headers
 rc=$?
 if (( rc != 0 )); then kubectl apply -f target/deployment/manifests/svc.yml; fi
 
-kubectl get rc river --no-headers
-rc=$?
-if (( rc != 0 )); then kubectl apply -f target/deployment/manifests/rc.yml; fi
+#kubectl get rc river --no-headers
+#rc=$?
+#if (( rc != 0 )); then kubectl apply -f target/deployment/manifests/rc.yml; fi
+
+kubectl apply -f target/deployment/manifests/rc.yml
 
 kubectl rolling-update river --image=127.0.0.1:5000/inu/river:${version}
 
