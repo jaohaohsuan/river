9e33c349b62f6de105298ee29118271fee5fe549
Henry Jao <jaohaohsuan@gmail.com>
2016-07-05 13:21:34 +0800

    add  more fields PROJECT_NAME & CONVERSATION_END_STATUS_TYPE_CD

--- src/main/scala/com/inu/river/service/XmlUploadService.scala
+++ src/main/scala/com/inu/river/service/XmlUploadService.scala
@@ -53,27 +53,35 @@ trait XmlUploadService extends Directives {
 
       val combined = ns.reduce(_ ++ _)
       val result = for {
-        node      <- getRecognizeTextNode(combined).right
-        roles     <- getRoles(node).right
-        start     <- getDateTime(combined, "START_DATETIME").right
-        end       <- getDateTime(combined, "END_DATETIME").right
-        length    <- getAudioDuration(combined).right
-        agentPhoneNo <- getAudioInfo(combined, "OPERATOR_PHONENUMBER").right
-        agentId   <- getUserInfo(combined, "USER_ID").right
-        agentName <- getUserInfo(combined, "USER_NAME").right
-        callDirection <- getConversationInfo(combined, "amivoice.common.direction").right
+        node            <- getRecognizeTextNode(combined).right
+        roles           <- getRoles(node).right
+        start           <- getDateTime(combined, "START_DATETIME").right
+        end             <- getDateTime(combined, "END_DATETIME").right
+        length          <- getAudioDuration(combined).right
+        agentPhoneNo    <- getAudioInfo(combined, "OPERATOR_PHONENUMBER").right
+        endStatus       <- getAudioInfo(combined, "CONVERSATION_END_STATUS_TYPE_CD").right
+        projectName     <- getAudioInfo(combined, "PROJECT_NAME").right
+        agentId         <- getUserInfo(combined, "USER_ID").right
+        agentName       <- getUserInfo(combined, "USER_NAME").right
+        callDirection   <- getConversationInfo(combined, "amivoice.common.direction").right
         customerPhoneNo <- getConversationInfo(combined, "amivoice.common.customer.phonenumber").right
-        customerGender <- getConversationInfo(combined, "amivoice.common.customer.gender").right
-        indexName <- asIndex(start).right
-        mixed     <- Right(roles.flatMap { r => r.sentences }.sortBy { case Sentence(_, _, begin +: _, _) => begin }).right
+        customerGender  <- getConversationInfo(combined, "amivoice.common.customer.gender").right
+        indexName       <- asIndex(start).right
+        mixed           <- Right(roles.flatMap { r => r.sentences }.sortBy { case Sentence(_, _, begin +: _, _) => begin }).right
       } yield {
         import org.json4s.JsonDSL._
-        val audioInfo: JObject = "length" -> length
-        val userInfo = ("agentId" -> agentId) ~~ ("agentName" -> agentName)
+        val audioInfo: JObject = ("length" -> length) ~~
+                                 ("endStatus" -> endStatus) ~~
+                                 ("projectName" -> projectName) ~~
+                                 ("agentPhoneNo" -> agentPhoneNo)
+
+        val userInfo = ("agentId" -> agentId) ~~
+                       ("agentName" -> agentName)
+
         val conversationInfo = ("callDirection" -> callDirection) ~~
-                               ("agentPhoneNo" -> agentPhoneNo) ~~
                                ("customerPhoneNo" -> customerPhoneNo) ~~
                                ("customerGender" -> customerGender)
+
         val extra = decomposeDate(start, end) ~~ audioInfo ~~ userInfo ~~ conversationInfo
         indexName :: toJson(roles).merge(toJson(Dialogs(mixed) :: Nil)).merge(toJson(Vtt(mixed) :: Nil)).merge(extra) :: HNil
       }
